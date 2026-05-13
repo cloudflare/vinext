30e81da836a28e474398524675660d5b8327df88
JamesbbBriz <jin1995115217@gmail.com>
2026-05-14 01:16:02 +0800

    Match Next route type import

--- packages/vinext/src/typegen.ts
+++ packages/vinext/src/typegen.ts
@@ -15,7 +15,7 @@ type ParamShape = Map<string, "string" | "string[]" | "string[]?">;
 
 const NEXT_ENV_FILE_CONTENT = `/// <reference types="next" />
 /// <reference types="next/image-types/global" />
-/// <reference path="./.next/types/routes.d.ts" />
+import "./.next/types/routes.d.ts";
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
