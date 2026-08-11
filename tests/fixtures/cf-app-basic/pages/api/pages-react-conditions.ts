6c4aa5dba4e3411336370dc7540fcc8d931ec0ae
James <james@eli.cx>
2026-08-11 05:50:29 +0100

    test(build): cover URL imports on Workers

--- tests/fixtures/cf-app-basic/pages/api/pages-react-conditions.ts
+++ tests/fixtures/cf-app-basic/pages/api/pages-react-conditions.ts
@@ -1,6 +1,10 @@
 import { getReactConditions } from "../../lib/react-conditions";
 import type { NextApiRequest, NextApiResponse } from "next";
 
+// Ported from Next.js:
+// test/e2e/react-version/pages/api/pages-api-edge-url-dep.js
+void import(new URL("./react-conditions.css", import.meta.url).href);
+
 export default function reactConditionsApi(_req: NextApiRequest, res: NextApiResponse) {
   res.json(getReactConditions());
 }
