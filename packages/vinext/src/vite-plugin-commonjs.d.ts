90c4f1338d28e552e70505a75c2a608a4874e412
James <james@eli.cx>
2026-08-18 10:39:59 +0100

    chore(deps): remove vite-plugin-commonjs

--- packages/vinext/src/vite-plugin-commonjs.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "vite-plugin-commonjs" {
-  import type { Plugin } from "vite";
-
-  export type CommonJsPluginOptions = {
-    [key: string]: unknown;
-  };
-
-  export default function commonjs(options?: CommonJsPluginOptions): Plugin;
-}
