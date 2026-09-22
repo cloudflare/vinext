30883e611b218ef2bdeaa840429808a1416ca4dc
James <james@eli.cx>
2026-09-22 23:45:31 +0100

    test(cache): exercise public force-dynamic query variants

--- examples/response-store-demo/app/query-public/page.tsx
+++ examples/response-store-demo/app/query-public/page.tsx
@@ -1,5 +1,7 @@
 // Ported from Next.js: test/e2e/app-dir/custom-cache-control/custom-cache-control.test.ts
 // https://github.com/vercel/next.js/blob/canary/test/e2e/app-dir/custom-cache-control/custom-cache-control.test.ts
+export const dynamic = "force-dynamic";
+
 export default async function QueryPublicPage({
   searchParams,
 }: {
